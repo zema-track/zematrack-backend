e4b0607afbd3a6ec1fbd2fc4039bbf9e855da324
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2025-09-28 23:40:08 +0300

    fix: implement custom CORS middleware to handle allowed origins

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,23 @@ const app: Application = express();
 // Middleware
 app.use(captureStartTime);
 app.use(logger);
-app.use(cors());
+
+const allowedOrigins = (process.env.ORIGIN_URLS || "").split(',').filter(origin => origin.trim() !== "");
+// CORS
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+}));
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
